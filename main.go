d8eefdace66071942fcb01d424f9dd47e669b47a
llh <liheng666@foxmail.com>
2017-08-09 01:19:37 +0800

    比对两个数据库的表结构，并自动修正  mysqldiff-go版本

---
+++ main.go
@@ -0,0 +1,188 @@
+package main
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"fmt"
+	"io/ioutil"
+	"encoding/json"
+	"regexp"
+)
+
+type mysqlDiff struct {
+	Conns               map[string]*sql.DB
+	successAddTables    []string
+	successRepairTables []string
+	errorAddTables      []string
+	errorRepairTables   []string
+	addTables           []string            // 需要添加的表
+	repairTables        map[string][]string // 需要修复的表
+}
+
+type DbConfig map[string]string
+
+func main() {
+	md := &mysqlDiff{
+		Conns:        make(map[string]*sql.DB),
+		repairTables: make(map[string][]string),
+	}
+	config, err := readJsonFile("./config.json")
+	checkErr(err)
+	//fmt.Println(config)
+	md.connDb(config)
+
+	md.contrastDd()
+
+	// 添加确实的表
+	md.addTable()
+
+	md.repairTable()
+
+	fmt.Printf("%v\n", md.addTables)
+	fmt.Printf("%v\n", md.repairTables)
+
+	//db, err := sql.Open("mysql", "root:123456@/go_test?charset=utf8")
+
+}
+
+func (md *mysqlDiff) connDb(config map[string]DbConfig) {
+	DSN1 := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8",
+		config["master"]["user"], config["master"]["pwd"], config["master"]["host"], config["master"]["port"], config["master"]["db"])
+	db1, err := sql.Open("mysql", DSN1)
+	checkErr(err)
+	md.Conns["master"] = db1
+
+	DSN2 := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8",
+		config["slave"]["user"], config["slave"]["pwd"], config["slave"]["host"], config["slave"]["port"], config["slave"]["db"])
+	db2, err := sql.Open("mysql", DSN2)
+	checkErr(err)
+	md.Conns["slave"] = db2
+}
+
+func (md *mysqlDiff) contrastDd() {
+	masterDb := md.Conns["master"]
+	master := readDatabaseTables(masterDb)
+
+	slaveDb := md.Conns["slave"]
+	slave := readDatabaseTables(slaveDb)
+
+	for _, masterTableName := range master {
+		if !in_slice(masterTableName, slave) {
+			md.addTables = append(md.addTables, masterTableName)
+		} else {
+			fieldDiff := md.fieldDiff(masterTableName)
+			if fieldDiff != nil {
+				md.repairTables[masterTableName] = fieldDiff
+			}
+		}
+	}
+}
+
+func (md *mysqlDiff) fieldDiff(tableName string) ([]string) {
+	var data []string
+	master := readTableField(md.Conns["master"], tableName)
+	slave := readTableField(md.Conns["slave"], tableName)
+
+	for _, masterField := range master {
+		if !in_slice(masterField, slave) {
+			data = append(data, masterField)
+		}
+	}
+
+	return data
+}
+
+func (md *mysqlDiff) getTableCreateSql(tableName string) string {
+	sql := "show create table " + tableName
+	rowsMaster, err := md.Conns["master"].Query(sql)
+	checkErr(err)
+	var str string
+	rowsMaster.Next()
+	rowsMaster.Scan(&str, &str)
+	return str
+}
+
+func (md *mysqlDiff) addTable() {
+	for _, name := range md.addTables {
+		sql := md.getTableCreateSql(name)
+		_, err := md.Conns["slave"].Query(sql)
+		checkErr(err)
+	}
+}
+
+func (md *mysqlDiff) repairTable() {
+	for tableName, fieldSlice := range md.repairTables {
+		createSql := md.getTableCreateSql(tableName)
+
+		for _, field := range fieldSlice {
+			pattern := fmt.Sprintf("`%s`.*?(,|\n)", field)
+			re, err := regexp.Compile(pattern)
+			checkErr(err)
+			sql := re.FindString(createSql)
+			if sql == "" {
+				continue
+			}
+
+			bytes := []rune(sql)
+			bytes = bytes[:len(bytes)-1]
+			sql = string(bytes)
+
+			sql = fmt.Sprintf("alter table %s add %s", tableName, sql)
+			_, err = md.Conns["slave"].Query(sql)
+			checkErr(err)
+		}
+	}
+}
+
+func in_slice(search string, sli []string) (bool) {
+	for _, str := range sli {
+		if search == str {
+			return true
+		}
+	}
+	return false
+}
+
+func readDatabaseTables(db *sql.DB) ([]string) {
+	var data []string
+	rowsMaster, err := db.Query("show tables")
+	checkErr(err)
+
+	for rowsMaster.Next() {
+		var str string
+		rowsMaster.Scan(&str)
+		data = append(data, str)
+	}
+	return data
+}
+
+func readTableField(db *sql.DB, tableName string) ([]string) {
+	var data []string
+	rowsMaster, err := db.Query("desc " + tableName)
+	checkErr(err)
+
+	for rowsMaster.Next() {
+		var str string
+		var str1 string
+		rowsMaster.Scan(&str, &str1, &str1, &str1, &str1, &str1)
+		data = append(data, str)
+	}
+	return data
+}
+
+func readJsonFile(filename string) (map[string]DbConfig, error) {
+	var config = make(map[string]DbConfig)
+	bytes, err := ioutil.ReadFile(filename)
+	checkErr(err)
+
+	err = json.Unmarshal(bytes, &config)
+	checkErr(err)
+	return config, nil
+}
+
+func checkErr(err error) {
+	if err != nil {
+		fmt.Printf("%v", err)
+		panic(err)
+	}
+}
